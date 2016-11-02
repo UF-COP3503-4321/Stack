650efec19917c0c267c148c8b4faf4af8b01d712
Luis (LT) Carbonell <lt.carbonell@gmail.com>
2016-11-02 18:11:28 -0400

    Define stack methods

---
+++ stack.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "stack.h"
+
+void Stack::push(int value) {
+  list.insertAt(value, topIndex);
+}
+
+int Stack::pop() {
+  int result = list.findAt(topIndex);
+  list.deleteAt(topIndex);
+  return result;
+}
+
+int Stack::top() {
+  int result = list.findAt(topIndex);
+  return result;
+}
+
+int Stack::size() {
+  return list.getSize();
+}
+
+bool Stack::isEmpty() {
+  if (list.getSize() == 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void Stack::print() {
+  // list.print();  Technically okay
+  Stack printStack = Stack();
+  while (!this.isEmpty()) {
+    cout << this.top();
+    printStack.push(this.pop());
+  }
+  while (!printStack.isEmpty()) {
+    this.push(printStack.pop());
+  }
+
+}
