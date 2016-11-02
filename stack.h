385c7be4bd546cc49e4fd62f545d57ae87fe332b
Luis (LT) Carbonell <lt.carbonell@gmail.com>
2016-11-02 17:54:21 -0400

    Create stack protoype class using a linkedlist

---
+++ stack.h
@@ -0,0 +1,21 @@
+#include "LinkedList.h"
+
+class Stack {
+  // Data members
+private:
+  LinkedList list;
+  int topIndex;
+  // methods
+public:
+  void push(int value); // insert item at top of Stack
+  int pop(); // Return and remove the top item in Stack
+  int top(); // Return the top item WITHOUT removing it
+  int size(); // Return the size of the Stack
+  void print();  // Print out the Stack
+  bool isEmpty(); // Return true if the stack is empty
+  // Constructor
+  Stack() {
+    link = LinkedList();
+    topIndex = 0;
+  }
+};
